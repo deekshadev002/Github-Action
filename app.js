5d8de7f1b564069bc142d0775ce752eb7daa2e64
deeksha <deeksha.pokhriyal002@gmail.com>
2025-08-07 08:41:12 +0000

    Saving changes before pulling

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
   res.send('🚀 Hello dev from GitHub Actions Deployment!');
+=======
+  res.send('🚀 Hello main from GitHub Actions Deployment!');
+>>>>>>> e6adf3e (Update app.js)
 });
 
 app.listen(port, () => {
